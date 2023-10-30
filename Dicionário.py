4f245ecb10f371fabcbad1ff220d9aabf16092f7
BiancaTS <53481713+BiancaTS@users.noreply.github.com>
2023-10-30 11:42:10 -0300

    Dicionário pessoas
    Teste de código em Python. Dicionário. Exercício de faculdade de Ciência da Computação. Paradígmas de Linguagem de Programação em Python - Python Básico - Tipos sequenciais e dicionários..

---
+++ Dicionário.py
@@ -0,0 +1,5 @@
+pessoas={1111:['nome 01'],2222:['nome 02'],3333:['nome 03'],4444:['nome 04']}
+pessoas
+{1111:['nome 01'],2222:['nome 02'],3333:['nome 03'],4444:['nome 04']}
+print('primeira pessoa = {}'.format(pessoas[1111]))
+print('quarta pessoa = {}'.format(pessoas[4444]))
